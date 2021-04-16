1559ae3392d7766cd96fb551a7f6cc95f2504d24
nggit <contact@anggit.com>
2021-04-16 18:25:56 +0700

    dukungan membaca koma, dukungan argument cli

--- terbilang/terbilang.py
+++ terbilang/terbilang.py
@@ -13,34 +13,44 @@ else:
     NORMAL = '\033[0m'
 
 class Terbilang:
-    def __init__(self, num=''):
-        self._num_str  = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas']
-        self._suffixes = [
+    def __init__(self, num='', sep=','):
+        self._num_str    = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas']
+        self._suffixes   = [
             'puluh', 'belas', ['', 'ratus'], ['', 'ribu', 'juta', 'miliar'], ['', 'triliun', 'septiliun', 'undesiliun', 'kuindesiliun', 'novemdesiliun']
         ]
-        self._result   = []
-        self.parse(num)
+        self._result     = []
+        self._separators = [',', '.']
+        self.parse(num, sep)
         try:
             self._input = raw_input
         except NameError:
             self._input = input
 
-    def getresult(self):
-        return ' '.join([r for r in self._result if r != '']).rstrip(', ').replace('satu ratus', 'seratus') \
-                                                                          .replace('satu ribu', 'seribu') \
-                                                                          .replace(';', '')
+    def getresult(self, result=[]):
+        if result:
+            return ' '.join([r for r in result if r != '']).rstrip(' ,').replace('satu ratus', 'seratus') \
+                                                                        .replace('satu ribu', 'seribu') \
+                                                                        .replace(';', '')
+        return ' '.join(self._result)
+
+    def filter_num(self, num=''):
+        for i in range(len(num)):
+            if ord(num[i]) < 48 or ord(num[i]) > 57:
+                num = num[:i] + ' ' + num[i + 1:]
+        return num.replace(' ', '')
 
     def spell(self, num=''):
         del self._result[:]
         for n in num:
-            if n == '0':
-                self._result.append('nol')
-            else:
-                self._result.append(self._num_str[int(n)])
+            if ord(n) >= 48 and ord(n) <= 57:
+                if n == '0':
+                    self._result.append('nol')
+                else:
+                    self._result.append(self._num_str[int(n)])
         return self
 
-    def parse(self, num=''): # FIXME: sederhanakan ini
-        num = str(num).strip()
+    def _read(self, num=''):
+        num = self.filter_num(num)
         if num != num.lstrip('0'):
             return self.spell(num)
         if len(num) > 72:
@@ -69,12 +79,46 @@ class Terbilang:
             num = num[len(num) - s_index * 12:].lstrip('0')
         return self
 
+    def parse(self, num='', sep=','):
+        if sep not in self._separators:
+            raise ValueError('Harap gunakan koma atau titik sebagai pemisah')
+        result = []
+        num    = str(num).strip(' ,.')
+        if num.find('-') == 0:
+            result.append('minus')
+        sep_pos = num.rfind(sep)
+        if sep_pos > 0:
+            result.append(self.getresult(self._read(num[:sep_pos])._result))
+            result.append('koma')
+            result.append(self.spell(num[sep_pos:]).getresult())
+        else:
+            sep_alt     = self._separators[self._separators.index(sep) ^ 1]
+            sep_alt_pos = num.find(sep_alt)
+            if num.count(sep_alt) == 1 and len(num[sep_alt_pos:]) != 4:
+                result.append(self.getresult(self._read(num[:sep_alt_pos])._result))
+                result.append('koma')
+                result.append(self.spell(num[sep_alt_pos:]).getresult())
+            else:
+                result.append(self.getresult(self._read(num)._result))
+        self._result[:] = result
+        return self
+
     """ markicob """
     def run(self):
         num = 0
+        sep = ','
+        for i in range(len(sys.argv)):
+            if sys.argv[i - 1] == '-n' or sys.argv[i - 1] == '--n':
+                num = sys.argv[i]
+            elif sys.argv[i - 1] == '-s' or sys.argv[i - 1] == '--s':
+                sep = sys.argv[i]
+            if sys.argv[i] == sys.argv[-1] and len(sys.argv) > 2:
+                print(self.parse(num, sep).getresult())
+                return None
         while num != '':
             num = self._input(' Masukkan angka (maks. 72 digit) -->|'.rjust(72, '-') + '\n')
-            print('%s%s%s' % (GREEN, self.parse(num).getresult(), NORMAL))
+            print('%s%s%s' % (GREEN, self.parse(num, sep).getresult(), NORMAL))
+        print('terbilang - https://github.com/nggit/terbilang\n\nPenggunaan:\n   python -m terbilang\n   python -m terbilang -n 1.000,00')
         return None
 
 if __name__ == '__main__':
